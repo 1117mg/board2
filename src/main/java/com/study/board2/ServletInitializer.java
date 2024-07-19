3e6661e08fe30a548889a7464bb53a11e1208dbb
DESKTOP-5QCQA0B\lucia <lucia_17@naver.com>
2024-07-19 15:08:18 +0900

    initial commit

---
+++ src/main/java/com/study/board2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.study.board2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Board2Application.class);
+    }
+
+}
