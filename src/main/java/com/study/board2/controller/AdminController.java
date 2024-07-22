19671ce1223cfa7b4efcf43900fceb17a4491163
DESKTOP-5QCQA0B\lucia <lucia_17@naver.com>
2024-07-22 12:21:17 +0900

    feat : Spring Security 관리자 가입, 로그인

--- src/main/java/com/study/board2/controller/AdminController.java
+++ src/main/java/com/study/board2/controller/AdminController.java
@@ -1,7 +1,12 @@
 package com.study.board2.controller;
 
+import com.study.board2.dto.JoinForm;
+import com.study.board2.dto.LoginForm;
+import com.study.board2.service.AdminService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +16,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 public class AdminController {
 
+    @Autowired
+    private AdminService adminService;
+
     @GetMapping("/auth/login")
-    public String loginForm(){
+    public String loginForm(Model model){
+        model.addAttribute("loginForm", new LoginForm());
         return "master/login";
     }
 
-    @PostMapping("/auth/login")
-    public String login(){
-        return "master/main";
+    @GetMapping("/auth/join")
+    public String joinForm(Model model){
+        model.addAttribute("joinForm", new JoinForm());
+        return "master/join";
+    }
+
+    @PostMapping("/auth/join")
+    public String join(JoinForm form){
+        adminService.register(form);
+        return "redirect:/master/auth/login";
     }
 
     @GetMapping("/main")
