bf006fbffe2e9d3d622763652551f82d5a0596bb
DESKTOP-5QCQA0B\lucia <lucia_17@naver.com>
2024-07-23 12:52:05 +0900

    feat : 게시판 조회수 style : 글목록 테이블 형식

--- src/main/java/com/study/board2/controller/BoardController.java
+++ src/main/java/com/study/board2/controller/BoardController.java
@@ -30,19 +30,27 @@ public class BoardController {
     // 게시판 글 목록
     @GetMapping("/{boardIdx}/posts")
     public String postList(@PathVariable("boardIdx") int boardIdx, Model model) {
-        List<Post> posts = postService.getPostsByBoardId(boardIdx);
-        model.addAttribute("posts", posts);
+        if(boardIdx==1){    // 공지사항 게시판
+            List<Post> posts = postService.getPostsByBoardId(boardIdx);
+            model.addAttribute("posts", posts);
+        }else{              // 1:1 문의 게시판
+            List<Post> qposts = postService.getQPostsByBoardId(boardIdx);
+            model.addAttribute("posts", qposts);
+        }
         return "front/postList";
     }
 
+    // 게시글 상세
     @GetMapping("/post/{postId}")
     public String postDetail(@PathVariable("postId") int postId, Model model) {
         Post post = postService.getPostById(postId);
+        post.setHits(postService.hit(postId));
         model.addAttribute("post", post);
         model.addAttribute("boardIdx", post.getBoardIdx());
         return "front/postDetail";
     }
 
+    // 게시글 등록 폼
     @GetMapping("/{boardIdx}/post/write")
     public String writePostForm(@PathVariable("boardIdx") int boardIdx, Model model) {
         Post post = new Post();
@@ -51,6 +59,7 @@ public class BoardController {
         return "front/writePost";
     }
 
+    // 게시글 등록
     @PostMapping("/{boardIdx}/post/write")
     public String writePost(@PathVariable("boardIdx") int boardIdx, @ModelAttribute Post post) {
         post.setBoardIdx(boardIdx);
@@ -58,6 +67,7 @@ public class BoardController {
         return "redirect:/front/board/" + boardIdx + "/posts";
     }
 
+    // 게시글 수정 폼
     @GetMapping("/{boardIdx}/post/{postId}/edit")
     public String editPostForm(@PathVariable("boardIdx") int boardIdx, @PathVariable("postId") int postId, Model model) {
         Post post = postService.getPostById(postId);
@@ -66,6 +76,7 @@ public class BoardController {
         return "front/editPost";
     }
 
+    // 게시글 수정
     @PostMapping("/{boardIdx}/post/{postId}/edit")
     public String editPost(@PathVariable("boardIdx") int boardIdx, @PathVariable("postId") int postId, @ModelAttribute Post post) {
         post.setIdx(postId);
@@ -74,6 +85,7 @@ public class BoardController {
         return "redirect:/front/board/" + boardIdx + "/posts";
     }
 
+    // 게시글 삭제
     @PostMapping("/post/{postId}/delete")
     public String deletePost(@PathVariable("postId") int postId) {
         Post post = postService.getPostById(postId);
