8f28e9a333b54410622fd078e2ace8e86f81581e
DESKTOP-5QCQA0B\lucia <lucia_17@naver.com>
2024-07-24 14:07:07 +0900

    feat : 1:1문의 게시판 crud(계층형)

--- src/main/java/com/study/board2/dto/Post.java
+++ src/main/java/com/study/board2/dto/Post.java
@@ -19,7 +19,7 @@ public class Post {
     private LocalDateTime regDate;
 
     // 계층 구조 관련 필드
-    private Integer parentIdx;  // 부모 글의 ID
+    private int parentIdx;  // 부모 글의 ID
     private int depth;          // 게시글 깊이
     private int sorts;       // 같은 깊이 내에서의 순서
 }
